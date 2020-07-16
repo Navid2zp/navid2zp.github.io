670b966ebb50fd0b2877e69cc7f0aee6d8298801
Navid Zarepak <navid2zp@gmail.com>
2020-07-16 20:15:23 +0430

    first commit

---
+++ src/Others/GlobalVariables.js
@@ -0,0 +1,364 @@
+/*
+MIT License
+
+Copyright (c) 2020 Fayaz Bin Salam
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+import {giftIcon} from './MainItems'
+
+export const skills = [
+    {
+        text: "Programming/Markup Languages",
+        arr: [
+            {
+                title: 'Go',
+                logo: '/icons/go.png',
+            },
+            {
+                title: 'Python',
+                logo: 'https://www.vectorlogo.zone/logos/python/python-icon.svg',
+            },
+            {
+                title: 'JavaScript',
+                logo: 'https://www.vectorlogo.zone/logos/javascript/javascript-icon.svg',
+            },
+            {
+                title: 'SQL',
+                logo: 'https://www.svgrepo.com/show/255832/sql.svg',
+            },
+            {
+                title: 'HTML',
+                logo: 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
+            },
+            {
+                title: 'PHP',
+                logo: 'https://www.vectorlogo.zone/logos/php/php-icon.svg',
+            },
+            {
+                title: 'Dart',
+                logo: 'https://www.vectorlogo.zone/logos/dartlang/dartlang-icon.svg',
+            },
+        ]
+    },
+    {
+        text: "Libraries/Frameworks",
+        arr: [
+            {
+                title: 'Django',
+                logo: 'https://img.icons8.com/material-outlined/24/000000/django.png'
+			},
+			{
+                title: 'Celery',
+                logo: 'https://docs.celeryproject.org/en/stable/_static/celery_512.png'
+			},
+			{
+                title: 'gin',
+                logo: 'https://raw.githubusercontent.com/gin-gonic/logo/master/color.png'
+			},
+            {
+                title: 'NodeJS',
+                logo: 'https://www.vectorlogo.zone/logos/nodejs/nodejs-icon.svg'
+            },
+            {
+                title: 'ExpressJS',
+                logo: 'https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg'
+            },
+            {
+                title: 'ReactJS',
+                logo: 'https://www.vectorlogo.zone/logos/reactjs/reactjs-icon.svg'
+            },
+            {
+                title: 'ElectronJS',
+                logo: 'https://www.vectorlogo.zone/logos/electronjs/electronjs-icon.svg'
+            },
+
+            {
+                title: 'NextJS',
+                logo: 'https://foxundermoon.gallerycdn.vsassets.io/extensions/foxundermoon/next-js/0.0.2/1507561739278/Microsoft.VisualStudio.Services.Icons.Default'
+            },
+            {
+                title: 'Flutter',
+                logo: 'https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg'
+            },
+            {
+                title: 'BS4',
+                logo: 'https://funthon.files.wordpress.com/2017/05/bs.png?w=772'
+            },
+            {
+                title: 'Numpy',
+                logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/NumPy_logo.svg/1200px-NumPy_logo.svg.png'
+            },
+            {
+                title: 'Matplotlib',
+                logo: 'https://matplotlib.org/_static/logo2_compressed.svg'
+            },
+            {
+                title: 'Bootstrap',
+                logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png'
+            },
+
+        ]
+    },
+    {
+        text: "Others",
+        arr: [
+            {
+                title: 'Git',
+                logo: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg'
+            },
+            {
+                title: 'GitHub',
+                logo: 'https://www.vectorlogo.zone/logos/github/github-icon.svg'
+            },
+            {
+                title: 'GitLab',
+                logo: 'https://www.vectorlogo.zone/logos/gitlab/gitlab-icon.svg'
+            },
+            {
+                title: 'Pycharm',
+                logo: '/icons/pycharm.png'
+            },
+            {
+                title: 'VS Code',
+                logo: 'https://cdn.worldvectorlogo.com/logos/visual-studio-code.svg'
+            },
+            {
+                title: 'Automation',
+                logo: 'https://image.flaticon.com/icons/svg/1035/1035688.svg'
+            },
+            {
+                title: 'NgInx',
+                logo: 'https://www.vectorlogo.zone/logos/nginx/nginx-icon.svg'
+            },
+            {
+                title: 'Linux',
+                logo: 'https://www.freepnglogos.com/uploads/linux-png/file-icons-flat-linux-svg-wikimedia-commons-6.png'
+            },
+
+        ]
+    },
+]
+
+export const projects = [
+	{
+        text: "WebApps",
+        arr: [
+
+            {
+                title: 'Arvix Cloud',
+                logo: '/icons/arvix-cloud.png',
+                desc: 'Hetzner cloud reseller panel with all the controls + a PAYG billing system',
+                link: 'arvix.cloud'
+            },
+            {
+                title: 'Uprino',
+                logo: '/icons/uprino.png',
+                desc: 'News personalization feed',
+            },
+            {
+                title: 'Ghalichin',
+                logo: '/icons/ghalichin.png',
+                desc: 'Create and preview rugs vritually before buying',
+                link: 'https://ghalichin.com/'
+            },
+            {
+                title: 'Arvix Studio',
+                logo: '/icons/arvix-studio.jpg',
+                desc: 'Development studio',
+            },
+        ]
+    },
+    {
+        text: "Android Apps",
+        arr: [
+            {
+                title: 'Gilak',
+                logo: 'https://lh3.googleusercontent.com/7wKogDf6SSCTRNVGfslRJmd6_Gq5eZxjqzKDNTJU6XpzSiXWtTaCWzU1HRDnhtJAyR0=s180-rw',
+                desc: 'A dictionary app for Gilaki language',
+                link: 'https://play.google.com/store/apps/details?id=studio.arvix.gilak'
+            },
+            {
+                title: 'IP Lookup',
+                logo: 'https://lh3.googleusercontent.com/Whh9vwuo7V5aig5hI6YM7AT0Xm_SlNab8WRqMJeuFT7cDH6OBHp9-vZISFYHCRJYf5dY=s180-rw',
+                desc: 'Simple app to lookup ip address information',
+                link: 'https://play.google.com/store/apps/details?id=studio.arvix.iplookup'
+            },
+        ]
+    },
+    {
+        text: "CLI",
+        arr: [
+            {
+                title: 'pubs',
+                logo: 'https://img.icons8.com/ios-filled/50/000000/powershell.png',
+                desc: 'A CLI tool to find/remove duplicate files supporting multi-core and different algorithms (MD5, SHA256, and XXHash)',
+                link: 'https://github.com/Navid2zp/dups'
+			},
+			{
+                title: 'ClassAttendanceManager',
+                logo: 'https://img.icons8.com/ios-filled/50/000000/powershell.png',
+                desc: 'Terminal based python script to manage students attendance in a class.',
+                link: 'https://github.com/Navid2zp/ClassAttendanceManager'
+			},
+        ]
+	},
+	{
+        text: "Bots",
+        arr: [
+            {
+                title: 'CSGOD',
+                logo: 'https://img.icons8.com/plasticine/100/000000/bot.png',
+                desc: 'A Telegram bot for CSGO. Get match results, upcoming events and set reminder for your favorite teams',
+                link: 'https://t.me/CSGODBot'
+			},
+			{
+                title: 'IP Lookup',
+                logo: 'https://img.icons8.com/plasticine/100/000000/bot.png',
+                desc: 'Simple Telegram bot to lookup any IPv4 address',
+                link: 'https://t.me/ArvixLookupBot'
+			},
+			{
+                title: 'Uprino error reporter',
+                logo: 'https://img.icons8.com/plasticine/100/000000/bot.png',
+                desc: 'Telegram bot to report critical errors',
+            },
+        ]
+    },
+    {
+        text: "Libraries",
+        arr: [
+            {
+                title: 'csplookup',
+                logo: 'https://www.vectorlogo.zone/logos/flutterio/flutterio-icon.svg',
+                desc: 'Dart client package for CSP Lookup API',
+                link: 'https://pub.dev/packages/csplookup'
+			},
+			{
+                title: 'go-wikidata',
+                logo: '/icons/go.png',
+                desc: 'Wikidata API bindings in go',
+                link: 'https://github.com/Navid2zp/go-wikidata'
+			},
+			{
+                title: 'easyreq',
+                logo: '/icons/go.png',
+                desc: 'Simplified HTTP requests and response unmarshaling',
+                link: 'https://github.com/Navid2zp/easyreq'
+			},
+			{
+                title: 'httptracer',
+                logo: '/icons/go.png',
+                desc: 'Simple HTTP tracer in Go',
+                link: 'https://github.com/Navid2zp/httptracer'
+			},
+			{
+                title: 'csplookup',
+                logo: '/icons/go.png',
+                desc: 'Go client package for CSP Lookup API',
+                link: 'https://github.com/Navid2zp/csplookup'
+			},
+			{
+                title: 'dbpreader',
+                logo: '/icons/go.png',
+                desc: 'Read dbpedia pages into golang type and structs. Simple and easy to use',
+                link: 'https://github.com/Navid2zp/dbpreader'
+			},
+			{
+                title: 'csplookup-py',
+                logo: 'https://www.vectorlogo.zone/logos/python/python-icon.svg',
+                desc: 'Python client library for CSP Lookup API',
+                link: 'https://github.com/Navid2zp/csplookup-py'
+			},
+			{
+                title: 'googlekg',
+                logo: '/icons/go.png',
+                desc: 'Google Knowledge Graph Search API bindings for golang',
+                link: 'https://github.com/Navid2zp/googlekg'
+			},
+			{
+                title: 'NumberToPersianWords',
+                logo: '/icons/go.png',
+                desc: 'Go package to turn numbers to Persian words',
+                link: 'https://github.com/Navid2zp/googlekg'
+			},
+			
+        ]
+    },
+    // {
+    //     text: "Others",
+    //     arr: [
+    //         {
+    //             title: 'CGPA_Calculator',
+    //             logo: giftIcon,
+    //             desc: 'CGPA Calculator made using C',
+    //             link: 'https://github.com/p32929/CGPA_Calculator'
+    //         },
+    //     ]
+    // }
+]
+
+export const contactInfos = [
+    {
+        text: "Contact me",
+        arr: [
+            {
+                title: 'Skype',
+                logo: 'https://image.flaticon.com/icons/svg/174/174869.svg',
+                link: 'navid2zp'
+            },
+            {
+                title: 'WhatsApp',
+                logo: 'https://image.flaticon.com/icons/svg/733/733585.svg',
+                link: 'https://api.whatsapp.com/send?phone=00989351120165'
+            },
+            {
+                title: 'Google Play',
+                logo: 'https://www.vectorlogo.zone/logos/google_play/google_play-icon.svg',
+                link: 'https://play.google.com/store/apps/developer?id=Navid2zp'
+            },
+            {
+                title: 'GitHub',
+                logo: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+                link: 'https://github.com/Navid2zp'
+			},
+			{
+                title: 'Telegram',
+                logo: 'https://img.icons8.com/color/144/000000/telegram-app.png',
+                link: 'https://t.me/Navid2zp'
+            },
+			{
+                title: 'Twitter',
+                logo: 'https://image.flaticon.com/icons/svg/733/733579.svg',
+                link: 'https://twitter.com/navid2zp'
+			},
+			{
+                title: 'Instagram',
+                logo: 'https://image.flaticon.com/icons/svg/2111/2111463.svg',
+                link: 'https://www.instagram.com/navid2zp/'
+			},
+			{
+                title: 'LinkedIn',
+                logo: 'https://image.flaticon.com/icons/svg/174/174857.svg',
+                link: 'https://www.linkedin.com/in/navid2zp/'
+            },
+        ]
+    },
+]
