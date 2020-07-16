6e9358cccedd58510135a5ce970ef41763a41d55
Navid Zarepak <navid2zp@gmail.com>
2020-07-16 20:19:51 +0430

    Updates

---
+++ precache-manifest.68c0dfb96b5ec930293cdc5bec600a6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63fa12de063c7f44dad99783492de28a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a222b3417b9d2c016f12",
+    "url": "/static/css/main.7fbd88ef.chunk.css"
+  },
+  {
+    "revision": "bbb54f2b7c74f99b132f",
+    "url": "/static/js/2.1d0ae48e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1d0ae48e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a222b3417b9d2c016f12",
+    "url": "/static/js/main.2d79412b.chunk.js"
+  },
+  {
+    "revision": "e9b78b3a42733e9071bb",
+    "url": "/static/js/runtime-main.17c7128f.js"
+  },
+  {
+    "revision": "5fe6d691308215841dda58fcc36c3efa",
+    "url": "/static/media/avatar2.5fe6d691.png"
+  }
+]);
